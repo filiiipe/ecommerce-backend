3fcf2e4e4d84b7d346869c2a2042242b44a9139a
Filipe Araujo <luiz4r4ujo@gmail.com>
2025-03-21 14:08:11 -0300

    feat: add service and controller layers

---
+++ src/main/java/com/store/ecommerce_backend/controller/CartController.java
@@ -0,0 +1,9 @@
+package com.store.ecommerce_backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+}
